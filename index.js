570098a3ab2a7ecf1a5f95fca96f8663f5963688
Sujatha Arunagiri <suja.lingam@gmail.com>
2023-02-28 11:00:30 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+
+app.get('/api', (req, res) => {
+  res.status(200).json({api: 'version 1'})
+})
+
+app.listen(3001, () => console.log('server started'))
+
